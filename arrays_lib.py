70559df5c509718e63662d5cc7ab5a7c21cfd04f
Timani1337 <idi_suda06@mail.ru>
2024-12-15 22:26:48 +0300

    Добавленна функция prod_1d для одномерного массива

--- arrays_lib.py
+++ arrays_lib.py
@@ -3,3 +3,9 @@ def sum_1d(array: list[int]) -> int:
     for i in range(len(array)):
         summ += array[i]
     return summ
+
+def prod_1d(array: list[int]) -> int:
+    prod = 1
+    for i in range(len(array)):
+        prod *= array[i]
+    return prod
