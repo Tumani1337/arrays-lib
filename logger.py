fe1afc12808a1d8f435cd976e40478142a38bfbd
kondak12 <ivan.kondakov.98@bk.ru>
2024-12-22 15:39:46 +0300

    logger обновлён

--- logger.py
+++ logger.py
@@ -1,7 +1,7 @@
-def log_action():
+def log_action(message):
     if "info.txt" not in "arrays_lib":
         with open("info.txt", "w") as information:
             pass
     else:
         with open("info.txt", "a") as information:
-            information.write(massage)
+            information.write(message)
