c6ab131ce64d79bb15d397e3950bef34934bff07
kondak12 <ivan.kondakov.98@bk.ru>
2024-12-22 16:22:47 +0300

    Модуль logger теперь работает

--- logger.py
+++ logger.py
@@ -1,7 +1,3 @@
 def log_action(message):
-    if "info.txt" not in "arrays_lib":
-        with open("info.txt", "w") as information:
-            pass
-    else:
-        with open("info.txt", "a") as information:
-            information.write(message)
+    with open("information.txt", "a") as information:
+        information.write(message)
--- main.py
+++ main.py
@@ -1,4 +1,33 @@
 from logger import log_action
 
-def main():
-    pass
+
+arr1 = [1, 2, 3, 4, 5]
+arr2 = [6, 7, 8, 9, 0]
+
+
+def sum_arrays(arr1, arr2):
+    result1 = []
+    for i in range(len(arr1)):
+        result1.append(arr1[i] + arr2[i])
+    log_action(f"{result1}" + "\r")
+    return result1
+
+
+def diff_arrays(arr1, arr2):
+    result2 = []
+    for i in range(len(arr1)):
+        result2.append(arr1[i] - arr2[i])
+    log_action(f"{result2}" + "\r")
+    return result2
+
+
+def prod_arrays(arr1, arr2):
+    result3 = []
+    for i in range(len(arr1)):
+        result3.append(arr1[i] * arr2[i])
+    log_action(f"{result3}" + "\r")
+    return result3
+
+sum_of_arrays = sum_arrays(arr1, arr2)
+diff_of_arrays = diff_arrays(arr1, arr2)
+prod_of_arrays = prod_arrays(arr1, arr2)
